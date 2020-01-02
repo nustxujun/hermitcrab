2f8427e594549129307ef36a0dd21bb0f87b16df
d9 <nustxujun@gmail.com>
2020-01-03 00:33:10 +0800

    fix bug

--- shaders/customization/chair_ps.hlsl
+++ shaders/customization/chair_ps.hlsl
@@ -19,8 +19,8 @@ half4 ps(PSInput input) : SV_TARGET
 	float4 albedo = lerp(lerp(ColoBase, ColorSeats, params.b), ColorMetal,params.g) * params.r;
 
 	half4 normal = calNormal(normalMap, linearSampler, input.uv,input.normal, input.tangent, input.binormal);
-
-	half3 color = directBRDF(0.5, 0.5, F0_DEFAULT, albedo, normal,-sundir, campos - input.worldPos);
+	float roughness = lerp(lerp(0.5, 0.2, params.b), 0.4, params.g);
+	half3 color = directBRDF(roughness, params.g, F0_DEFAULT, albedo, normal,-sundir, campos - input.worldPos);
 
 	return half4(color, 1);
 }
