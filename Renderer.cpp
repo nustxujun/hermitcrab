644efa0af401e031ae70cd1656685f22bb2c4308
jun xu <xujun02@corp.netease.com>
2020-01-15 10:23:34 +0800

    add sampler

--- Renderer.cpp
+++ Renderer.cpp
@@ -787,6 +787,19 @@ void Renderer::initResources()
 	mConstantBufferAllocator = ConstantBufferAllocator::create();
 
 	auto shader = compileShaderFromFile("shaders/gen_mips.hlsl","main", SM_CS);
+	shader->registerStaticSampler({
+		D3D12_FILTER_MIN_MAG_MIP_POINT,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		0,0,
+		D3D12_COMPARISON_FUNC_NEVER,
+		D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK,
+		0,
+		D3D12_FLOAT32_MAX,
+		0,0,
+		D3D12_SHADER_VISIBILITY_ALL
+	});
 	mMipmapGen = createComputePipelineState(shader);
 }
 
